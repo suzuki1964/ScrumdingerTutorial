ac139edda649546420e3a2bc216833ef40234d92
Kazuko Suzuki <suzuki1964@mac.com>
2022-03-27 09:42:53 -0500

    Initial Commit

---
+++ Shared/ScrumdingerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTutorialApp.swift
+//  Shared
+//
+//  Created by Kazuko Suzuki on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

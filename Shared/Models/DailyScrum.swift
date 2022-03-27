8b2a5081a3911730e96ceef1147c421dd5d7a557
Kazuko Suzuki <suzuki1964@mac.com>
2022-03-27 13:17:03 -0500

    Created CardView

---
+++ Shared/Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  ScrumdingerTutorial (iOS)
+//
+//  Created by Kazuko Suzuki on 3/27/22.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}

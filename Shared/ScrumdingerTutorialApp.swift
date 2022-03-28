b1626f348b5b03840ecbb76fed7b4c6acb42f294
Kazuko Suzuki <suzuki1964@mac.com>
2022-03-28 09:31:50 -0500

    Created navigation hierarchy

--- Shared/ScrumdingerTutorialApp.swift
+++ Shared/ScrumdingerTutorialApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ScrumdingerTutorialApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.sampleData)
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
